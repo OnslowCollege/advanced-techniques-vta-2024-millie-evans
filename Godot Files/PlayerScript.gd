a062ab922631936ab2ecbd67fc7033c09845c2f2
SomeKiwiOnslow <millie.evans@student.onslow.school.nz>
2024-06-24 09:45:10 +1200

    Started Time Travel
    Added the basic time travel - currently the layers do not hide after pressing the key - but collision does work

--- Godot Files/PlayerScript.gd
+++ Godot Files/PlayerScript.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const PAST = 1
+const PRESENT = 2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,15 +16,28 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		if Input.is_action_just_pressed("time_travel"):
+			# Get the current collision layers and masks and change them depending
+			# On the time the player is currently in
+			if collision_layer == 1 and collision_mask == 1:
+				print("The player is now in the past")
+				collision_layer = 2
+				collision_mask = 2
+			elif collision_layer == 2 and collision_mask == 2:
+				print("The player is now in the present")
+				collision_layer = 1
+				collision_mask = 1
+			else:
+				print("Error in time swapping")
 
 	move_and_slide()
