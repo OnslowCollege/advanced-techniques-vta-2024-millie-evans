00db0e87dc901a13670a6e49631f41c396b0e751
SomeKiwiOnslow <millie.evans@student.onslow.school.nz>
2024-06-26 12:35:10 +1200

    Continued the stuck checker
    It still doesn't work - but i'm getting closer

--- Godot Files/PlayerScript.gd
+++ Godot Files/PlayerScript.gd
@@ -5,21 +5,33 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const PAST = 1
 const PRESENT = 2
+var stuck_checker = preload("res://StuckChecker.tscn")
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func change_time(time):
-	collision_layer = time
-	collision_mask = time
-	if time == 1:
-		$"../TileMapPast".hide()
-		$"../TileMapPresent".show()
-	elif time == 2:
-		$"../TileMapPresent".hide()
-		$"../TileMapPast".show()
+func change_time(layer):
+	var stuck_checker_instance = stuck_checker.instantiate()
+	add_child(stuck_checker_instance)
+	print(stuck_checker_instance.global_position, position)
+	stuck_checker_instance.collision_layer = 1
+	stuck_checker_instance.collision_mask = 1
+	var is_stuck = stuck_checker_instance.get_overlapping_bodies()
+	stuck_checker_instance.queue_free()
+	print(is_stuck)
+	if is_stuck:
+		print("a")
 	else:
-		print("Error with time swapping in function")
+		collision_layer = layer
+		collision_mask = layer
+		if layer == 1:
+			$"../TileMapPast".hide()
+			$"../TileMapPresent".show()
+		elif layer == 2:
+			$"../TileMapPresent".hide()
+			$"../TileMapPast".show()
+		else:
+			print("Error with time swapping in function")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -34,10 +46,8 @@ func _physics_process(delta):
 		# Get the current collision layers and masks and change them depending
 		# On the time the player is currently in
 		if collision_layer == 1 and collision_mask == 1:
-			print("The player is now in the past")
 			change_time(2)
 		elif collision_layer == 2 and collision_mask == 2:
-			print("The player is now in the present")
 			change_time(1)
 		else:
 			print("Error in time swapping")
