3df3b3aa909e40b27e966a6b83eee53fa7e459f9
SomeKiwiOnslow <millie.evans@student.onslow.school.nz>
2024-06-27 11:53:31 +1200

    Moved the stuck checker into the player scene and added some comments

--- Godot Files/PlayerScript.gd
+++ Godot Files/PlayerScript.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
 
-const SPEED = 250.0
-const JUMP_VELOCITY = -250.0
-const PAST = 1
-const PRESENT = 2
+const SPEED = 250.0 # Speed the player moves at when using the movement keys
+const JUMP_VELOCITY = -250.0 # Speed the player jumps at
+const PAST = 2 # Layer the past is on
+const PRESENT = 1 # Layer the present is on
 
 func _ready():
+	# Set the stuck checker layer to fix a bug
+	# where the first time you tried changing time,
+	# it wouldn't work
 	$StuckChecker.collision_layer = 3
 	$StuckChecker.collision_mask = 3
 
@@ -14,25 +17,35 @@ func _ready():
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func change_time(layer):
+	# Set the stuck checker layer to the layer you are going to change to
 	$StuckChecker.collision_layer = layer
 	$StuckChecker.collision_mask = layer
+	# Check what is colliding with the checker - ignore the player
 	var is_stuck = $StuckChecker.get_overlapping_bodies()
 	is_stuck.erase($".")
-	print(is_stuck)
+	# Reset the stuck checker layer
 	$StuckChecker.collision_layer = 3
 	$StuckChecker.collision_mask = 3
 	if is_stuck:
+		# Print to the log for debugging
+		print(is_stuck)
 		print("Player cannot change layer")
 	else:
+		# Set the collision to the new layer
 		collision_layer = layer
 		collision_mask = layer
-		if layer == 1:
+		if layer == PRESENT:
+			print("Now in present")
+			# Show the present tilemap
 			$"../TileMapPast".hide()
 			$"../TileMapPresent".show()
-		elif layer == 2:
+		elif layer == PAST:
+			print("Now in past")
+			# Show the past tilemap
 			$"../TileMapPresent".hide()
 			$"../TileMapPast".show()
 		else:
+			# Print an error if on another layer
 			print("Error with time swapping in function")
 
 func _physics_process(delta):
@@ -46,12 +59,13 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("time_travel"):
 		# Get the current collision layers and masks and change them depending
-		# On the time the player is currently in
-		if collision_layer == 1 and collision_mask == 1:
-			change_time(2)
-		elif collision_layer == 2 and collision_mask == 2:
-			change_time(1)
+		# On the laer the player is currently on
+		if collision_layer == PAST and collision_mask == PAST:
+			change_time(PRESENT)
+		elif collision_layer == PRESENT and collision_mask == PRESENT:
+			change_time(PAST)
 		else:
+			# Print an error if on another layer
 			print("Error in time swapping")
 
 	# Get the input direction and handle the movement/deceleration.
