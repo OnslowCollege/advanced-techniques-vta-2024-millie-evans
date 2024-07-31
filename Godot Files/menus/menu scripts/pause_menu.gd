ad3f351b12642ea715d01f57f016a0445da3ed85
SomeKiwiOnslow <millie.evans@student.onslow.school.nz>
2024-07-31 13:08:51 +1200

    Continued Menus
    Fixed some menu bugs - and applied textures to menu buttons

--- Godot Files/menus/menu scripts/pause_menu.gd
+++ Godot Files/menus/menu scripts/pause_menu.gd
@@ -3,6 +3,7 @@ extends Control
 
 func _on_quit_menu_button_pressed():
 	get_tree().change_scene_to_file("res://menus/main_menu.tscn")
+	Engine.time_scale = 1
 
 
 func _on_quit_desktop_button_pressed():
