f28a05e72a0879e0ae955922c5a8acb80db99263
SomeKiwiOnslow <millie.evans@student.onslow.school.nz>
2024-08-12 08:49:34 +1200

    Level Select Menu and Level Design
    Started designing the level and added a level selection menu - with only one out of the three working due to time constraints

--- Godot Files/menus/menu scripts/main_menu.gd
+++ Godot Files/menus/menu scripts/main_menu.gd
@@ -3,6 +3,16 @@ extends Control
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://urbanlevel.tscn")
 
-
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_levels_button_pressed():
+	$MainMenuHbox.hide()
+	$LevelsHbox.show()
+
+func _on_levels_back_button_pressed():
+	$MainMenuHbox.show()
+	$LevelsHbox.hide()
+
+func _on_level_1_button_pressed():
+	get_tree().change_scene_to_file("res://urbanlevel.tscn")
